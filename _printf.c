7beb178a3680184511376011bc90ad5fe0a8b01f
junluck <junaindavidsvickerman@gmail.com>
2022-10-18 07:00:18 -0700

    _printf

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_printf-Entry point for the _printf function
+ *
+ *@format:format of the string (%c or %s)
+ *
+ *Description: Prints out string
+ *
+ *Return: Always 0.
+ */
+int _printf(const char *format, ...)
